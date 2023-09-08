e203054b72f37eae9e7f547371c57a38543a1487
Ezequiel <ezequielezequiel9@gmail.com>
2023-09-08 14:55:47 -0300

    first commit

---
+++ src/pages/loser/index.ts
@@ -0,0 +1,15 @@
+export function initResultadoLoser(params) {
+  const div = document.createElement("div");
+  div.innerHTML = `
+        
+  
+
+      <perdiste-comp></perdiste-comp>
+    
+    `;
+  const buttonEvent = div.querySelector(".mi-button");
+  buttonEvent?.addEventListener("click", () => {
+    params.goTo("/instrucciones");
+  });
+  return div;
+}
